acb95a99ff40021c3cc2786d54676af233d53255
Alexander Justo <alexanderpjy934320312@gmail.com>
2026-05-13 11:10:20 -0500

    fix: troubleshoot profile

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,43 +1,59 @@
 import { Routes } from '@angular/router';
+import { ProfileSettingsView } from './account/presentation/views/profile-settings-view/profile-settings-view';
 
 export const routes: Routes = [
   {
     path: 'dashboard',
-    loadComponent: () => import('./shared/presentation/views/dashboard/dashboard').then(m => m.DashboardViewComponent)
+    loadComponent: () =>
+      import('./shared/presentation/views/dashboard/dashboard').then(
+        (m) => m.DashboardViewComponent,
+      ),
   },
   {
     path: 'boton-panico',
-    loadComponent: () => import('./emergencies/presentation/views/emergencies-view/emergencies-view').then(m => m.EmergenciesViewComponent)
+    loadComponent: () =>
+      import('./emergencies/presentation/views/emergencies-view/emergencies-view').then(
+        (m) => m.EmergenciesViewComponent,
+      ),
   },
   {
     path: 'mapa-riesgo',
-    loadComponent: () => import('./incidents/presentation/views/risk-map-view/risk-map-view').then(m => m.RiskMapViewComponent)
+    loadComponent: () =>
+      import('./incidents/presentation/views/risk-map-view/risk-map-view').then(
+        (m) => m.RiskMapViewComponent,
+      ),
   },
   {
     path: 'reportes',
-    loadComponent: () => import('./incidents/presentation/views/report-views/report-views').then(m => m.ReportesComponent)
+    loadComponent: () =>
+      import('./incidents/presentation/views/report-views/report-views').then(
+        (m) => m.ReportesComponent,
+      ),
   },
   {
     path: 'crear-reporte',
-    loadComponent: () => import('./incidents/presentation/views/create-report/create-report').then(m => m.CrearReporteComponent)
+    loadComponent: () =>
+      import('./incidents/presentation/views/create-report/create-report').then(
+        (m) => m.CrearReporteComponent,
+      ),
   },
-  {
-    path: 'comunidades',
-    loadComponent: () => import('./communities/presentation/views/community-setup-view/community-setup-view').then(m => m.CommunitySetupViewComponent)
-  },
+
   {
     path: 'perfil',
-    loadComponent: () => import('./account/presentation/views/profile-view/profile-view').then(m => m.ProfileViewComponent)
+    loadComponent: () => import('./account/presentation/views/profile-settings-view/profile-settings-view').then(m => m.ProfileSettingsView)
   },
   {
     path: 'configuracion',
-    loadComponent: () => import('./account/presentation/views/setting-view/setting-view').then(m => m.SettingsViewComponent)
+    loadComponent: () =>
+      import('./account/presentation/views/setting-view/setting-view').then(
+        (m) => m.SettingsViewComponent,
+      ),
   },
 
   {
     // Ruta por defecto: Redirige al al dashboard al abrir la app
     path: '',
     redirectTo: 'dashboard',
-    pathMatch: 'full'
-  }
+    pathMatch: 'full',
+  },
 ];
