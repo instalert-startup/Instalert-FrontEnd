a16628564c5b74a2bfb712e2059539665dee386d
Jhoan Darner Janampa <jhoan.janamp@gmail.com>
2026-06-02 18:22:57 -0500

    feat(environment): update environment configuration and add API endpoints

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,1 +1,11 @@
-export const environment = {};
+export const environment = {
+  production: false,
+  serverBaseUrl: 'http://localhost:3000',
+  apiBasePath: '/api/v1',
+  usersEndpointPath: '/users',
+  incidentsEndpointPath: '/incidents',
+  emergenciesEndpointPath: '/emergencies',
+  riskZonesEndpointPath: '/risk-zones',
+  trustedContactsEndpointPath: '/trusted-contacts',
+  supportRequestsEndpointPath: '/support-requests',
+};
