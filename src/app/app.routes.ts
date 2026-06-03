3e6c9b77092d2d83d3ecf43d2b94449ae3931fbb
Jhoan Darner Janampa <jhoan.janamp@gmail.com>
2026-06-03 00:45:43 -0500

    feat(routes): restructure routing to include login and layout components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,59 +1,67 @@
 import { Routes } from '@angular/router';
-import { ProfileSettingsView } from './account/presentation/views/profile-settings-view/profile-settings-view';
 
 export const routes: Routes = [
   {
-    path: 'dashboard',
+    path: 'login',
     loadComponent: () =>
-      import('./shared/presentation/views/dashboard/dashboard').then(
-        (m) => m.DashboardViewComponent,
-      ),
+      import('./account/presentation/views/login-view/login-view').then((m) => m.LoginView),
   },
   {
-    path: 'boton-panico',
-    loadComponent: () =>
-      import('./emergencies/presentation/views/emergencies-view/emergencies-view').then(
-        (m) => m.EmergenciesViewComponent,
-      ),
-  },
-  {
-    path: 'mapa-riesgo',
-    loadComponent: () =>
-      import('./incidents/presentation/views/risk-map-view/risk-map-view').then(
-        (m) => m.RiskMapViewComponent,
-      ),
-  },
-  {
-    path: 'reportes',
-    loadComponent: () =>
-      import('./incidents/presentation/views/report-views/report-views').then(
-        (m) => m.ReportesComponent,
-      ),
-  },
-  {
-    path: 'crear-reporte',
-    loadComponent: () =>
-      import('./incidents/presentation/views/create-report/create-report').then(
-        (m) => m.CrearReporteComponent,
-      ),
-  },
-
-  {
-    path: 'perfil',
-    loadComponent: () => import('./account/presentation/views/profile-settings-view/profile-settings-view').then(m => m.ProfileSettingsView)
-  },
-  {
-    path: 'configuracion',
-    loadComponent: () =>
-      import('./account/presentation/views/setting-view/setting-view').then(
-        (m) => m.SettingsViewComponent,
-      ),
-  },
-
-  {
-    // Ruta por defecto: Redirige al al dashboard al abrir la app
     path: '',
-    redirectTo: 'dashboard',
-    pathMatch: 'full',
+    loadComponent: () =>
+      import('./shared/presentation/components/layout/layout').then((m) => m.Layout),
+    children: [
+      {
+        path: 'dashboard',
+        loadComponent: () =>
+          import('./shared/presentation/views/dashboard/dashboard').then(
+            (m) => m.DashboardViewComponent,
+          ),
+      },
+      {
+        path: 'boton-panico',
+        loadComponent: () =>
+          import('./emergencies/presentation/views/emergencies-view/emergencies-view').then(
+            (m) => m.EmergenciesViewComponent,
+          ),
+      },
+      {
+        path: 'mapa-riesgo',
+        loadComponent: () =>
+          import('./incidents/presentation/views/risk-map-view/risk-map-view').then(
+            (m) => m.RiskMapViewComponent,
+          ),
+      },
+      {
+        path: 'reportes',
+        loadComponent: () =>
+          import('./incidents/presentation/views/report-views/report-views').then(
+            (m) => m.ReportesComponent,
+          ),
+      },
+      {
+        path: 'crear-reporte',
+        loadComponent: () =>
+          import('./incidents/presentation/views/create-report/create-report').then(
+            (m) => m.CrearReporteComponent,
+          ),
+      },
+      {
+        path: 'perfil',
+        loadComponent: () =>
+          import('./account/presentation/views/profile-settings-view/profile-settings-view').then(
+            (m) => m.ProfileSettingsView,
+          ),
+      },
+      {
+        path: 'configuracion',
+        loadComponent: () =>
+          import('./account/presentation/views/setting-view/setting-view').then(
+            (m) => m.SettingsViewComponent,
+          ),
+      },
+      { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+    ],
   },
+  { path: '**', redirectTo: 'login' },
 ];
