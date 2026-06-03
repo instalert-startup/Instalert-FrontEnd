4f9b303aa9a124d7234e4bb26a7ffc15d88718e0
Jhoan Darner Janampa <jhoan.janamp@gmail.com>
2026-06-03 00:20:50 -0500

    feat(environment): update environment configuration and add API endpoints

--- src/app/emergencies/infrastructure/emergency-api.ts
+++ src/app/emergencies/infrastructure/emergency-api.ts
@@ -1,11 +1,18 @@
-import { Injectable } from '@angular/core';
+import { Injectable, inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 import { PermissionState } from '../domain/emergency-alert.entity';
+import { environment } from '../../../environments/environment';
 
 @Injectable({ providedIn: 'root' })
 export class EmergencyApi {
+  private http = inject(HttpClient);
+  private url = `${environment.serverBaseUrl}${environment.apiBasePath}${environment.emergenciesEndpointPath}`;
+
   private readonly PERM_KEY = 'instalert_location_permission';
   private readonly LOC_KEY = 'instalert_location_confirmed';
 
+  // localStorage (permisos)
   getPermissionStatus(): PermissionState {
     return (localStorage.getItem(this.PERM_KEY) as PermissionState) || 'pending';
   }
@@ -20,5 +27,14 @@ export class EmergencyApi {
 
   saveLocationConfirmed(status: boolean): void {
     localStorage.setItem(this.LOC_KEY, String(status));
+  }
+
+  // HTTP (historial)
+  getEmergencies(): Observable<any[]> {
+    return this.http.get<any[]>(this.url);
+  }
+
+  createEmergency(emergency: any): Observable<any> {
+    return this.http.post<any>(this.url, emergency);
   }
 }
