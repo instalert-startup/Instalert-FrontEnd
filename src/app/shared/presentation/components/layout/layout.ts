6d2dcaf2607996d978d264c76a8cd6440de9db70
Piero Molina <molinna.leo@gmail.com>
2026-06-12 07:36:40 +0200

    feat(sidebar): add i18n translations

--- src/app/shared/presentation/components/layout/layout.ts
+++ src/app/shared/presentation/components/layout/layout.ts
@@ -3,7 +3,8 @@ import {MatSidenavModule} from '@angular/material/sidenav';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatListModule} from '@angular/material/list';
 import {RouterModule} from '@angular/router';
-import {LanguageSwitcher} from '../language-switcher/language-switcher';
+import { LanguageSwitcher } from '../language-switcher/language-switcher';
+import { TranslatePipe } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-layout',
@@ -12,16 +13,16 @@ import {LanguageSwitcher} from '../language-switcher/language-switcher';
     MatToolbarModule,
     MatListModule,
     RouterModule,
-    LanguageSwitcher
+    LanguageSwitcher,
+    TranslatePipe,
   ],
   templateUrl: './layout.html',
-  styleUrl: './layout.css'
+  styleUrl: './layout.css',
 })
 /**
  * Shared presentation component orchestrating the application shell.
  * It contains the sidebar, header, and router outlet.
  */
 export class Layout {
-
   title = '';
 }
