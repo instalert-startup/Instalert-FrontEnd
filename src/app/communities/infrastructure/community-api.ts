6860eece82828d252f85ad209b644027b984a2ed
Jhoan Darner Janampa <jhoan.janamp@gmail.com>
2026-06-30 17:19:45 -0500

    feat: update CommunityApi to use environment variables and add community management methods

--- src/app/communities/infrastructure/community-api.ts
+++ src/app/communities/infrastructure/community-api.ts
@@ -1,16 +1,28 @@
 import { inject, Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { TrustedContact } from '../domain/trusted-contact.entity';
+import { environment } from '../../../environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class CommunityApi {
   private http = inject(HttpClient);
-  private baseUrl = 'https://instalert-api-zdxk.onrender.com/api/v1/users';
+  private baseUrl = `${environment.serverBaseUrl}${environment.apiBasePath}`;
 
-  getUsers(): Observable<TrustedContact[]> {
-    return this.http.get<TrustedContact[]>(this.baseUrl);
+  getUsers(): Observable<any[]> {
+    return this.http.get<any[]>(`${this.baseUrl}/users`);
+  }
+
+  getCommunities(): Observable<any[]> {
+    return this.http.get<any[]>(`${this.baseUrl}/communities`);
+  }
+
+  createCommunity(community: any): Observable<any> {
+    return this.http.post<any>(`${this.baseUrl}/communities`, community);
+  }
+
+  deleteCommunity(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.baseUrl}/communities/${id}`);
   }
 }
